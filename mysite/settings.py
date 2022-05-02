843ec3b4a50ad4df10d1a0c7693dedb1a9eada55
Pedro-Tuto <pedropb97@gmail.com>
2022-05-02 16:07:04 -0300

    AAdding and configuring static files + CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    "/home/tuto/Desktop/Code/blog/static",
+]
+
+'''STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder'
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]'''
+
