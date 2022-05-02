7c3e4fad667ca947775496e191b631d4a8301e07
Pedro-Tuto <pedropb97@gmail.com>
2022-05-02 15:15:25 -0300

    Adding query sets

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    all_posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    print(all_posts)
+    return render(request, 'blog/post_list.html', {'all_posts':all_posts})
 
 
     
